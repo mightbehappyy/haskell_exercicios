9d4bc5f61d00a6da7b033233588e120a6270e5d3
mightbehappyy <97134972+mightbehappyy@users.noreply.github.com>
2022-09-23 01:12:58 -0300

    Create exercicio1.hs

---
+++ exercicio1.hs
@@ -0,0 +1,41 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+fatorial n = foldr (+) 0 [1..n]
+
+
+aux 0 = 0
+aux 1 = 1
+aux n = aux (n-1) + aux(n-2)
+
+fib n = [aux n| n <- [1..n]]
+
+
+tres n = [x | x <- [0,3..n]]
+
+
+mult23 = [x | x <- [1..20], mod x 2 == 0 || mod x 3 == 0]
+
+listalista =[[n] | n <- [1..5]]
+
+
+repetir = [replicate n  1 | n <- [1..5]]
+
+
+matrix  = [(x,y) | x <- [1..3], y <- [3,2..1]]
+
+inserirord n [] = [n]
+inserirord n (x:xs)
+        | n <= x = n : x: xs
+        | otherwise = x : inserirord n xs
+
+
+insere n l
+        | elem n l == False = inserirord n l
+        | otherwise = l
+
+
+
+retornaSup n (x:xs)
+        | n < x = x:xs
+        | otherwise = retornaSup n xs
+
+retornaSup2 n (x:xs) = [x | x <- x:xs, n < x]
